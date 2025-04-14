f087f1ec946168915a197b4bbba3c93dd8dd324e
Sourav Bhowal <souravbhowal15@gmail.com>
2025-04-14 13:33:37 +0530

    final

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,13 +4,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <main className="flex h-screen items-center justify-center p-4">
-      <SignIn
-        appearance={{
-          elements: {
-            footer: "hidden",
-          },
-        }}
-      />
+      <SignIn />
     </main>
   );
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,13 +4,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <main className="flex h-screen items-center justify-center p-4">
-      <SignUp
-        appearance={{
-          elements: {
-            footer: "hidden",
-          },
-        }}
-      />
+      <SignUp />
     </main>
   );
 }
